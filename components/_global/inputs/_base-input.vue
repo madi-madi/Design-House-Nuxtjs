95100b9739036a90847dd352ec2c90ab8f524f52
madi-madi <ibrahim.s.m.2010@gmail.com>
2020-05-05 23:19:24 +0300

    create component base input

---
+++ components/_global/inputs/_base-input.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input
+      :type="inputType"
+      :value="value"
+      :readonly="readonly"
+      :placeholder="placeholder"
+      :class="[{ 'is-invalid ': form.errors.has(field) }, customClass]"
+      @input="handleInput"
+    />
+    <has-error :form="form" :field="field"></has-error>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    form: {type: Object,required: true},
+    field: {type: String,required: true},
+    value: {type: String,default: ''},
+    placeholder: {type: String,default: ''},
+    customClass: {type: String,default: ''},
+    inputType: {type: String,default: 'text'},
+    readonly: {type: Boolean,default: false}
+  },
+
+  methods: {
+    handleInput(e) {
+      this.$emit('input', e.target.value);
+    }
+  },
+
+  mounted() {
+    //console.log(this.$attrs);
+  }
+};
+</script>
+
+<style></style>
