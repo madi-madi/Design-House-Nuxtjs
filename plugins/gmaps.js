7c84ce783deccae6bec59fb9026c9bd65e22c05c
madi-madi <ibrahim.s.m.2010@gmail.com>
2020-05-10 14:06:39 +0300

    key map

--- plugins/gmaps.js
+++ plugins/gmaps.js
@@ -3,7 +3,7 @@ import * as VueGoogleMaps from 'vue2-google-maps-withscopedautocomp';
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyCSCmTpjsznfbYTsfn_MzoaQD2or8uu2vo',
+    key: 'keeeeeeey',
     libraries: 'places'
   }
 });
